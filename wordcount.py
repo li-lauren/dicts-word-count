2f6b9ece0285d82ed2150703837b29b13afba0dc
li-lauren <laurenlidev@gmail.com>
2020-10-05 13:17:29 -0600

    Complete wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,12 @@
 # put your code here.
+
+text = open('test.txt').read()
+text = text.split()
+
+dict_words = {}
+
+for word in text:
+    dict_words[word] = dict_words.get(word, 0) + 1
+
+for key, value in dict_words.items():
+    print(key, value)
