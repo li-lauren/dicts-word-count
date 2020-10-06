44e49155b132648e79172dab1021cfa0ca087cb0
li-lauren <laurenlidev@gmail.com>
2020-10-05 20:13:28 -0600

    Sort output by word count and then by word

--- wordcount.py
+++ wordcount.py
@@ -18,7 +18,7 @@ for word in text:
 print(cnt)
 
 # for key, value in dict_words.items():
-#     print(key, value)
+    print(key, value)
 
 # sort output by words alphabetically
 for key, value in sorted(dict_words.items()):
@@ -27,4 +27,11 @@ for key, value in sorted(dict_words.items()):
 # sort output by word count
 for key, value in sorted(dict_words.items(), key=lambda item: item[1]):
     print(key, value)
+
+# sort the output by word count, descending, then by word, ascending
+sorted_dict = sorted(dict_words.items())
+for key, value in sorted(sorted_dict, key=lambda item: item[1], reverse=True):
+    print(key, value)
+
+
 
